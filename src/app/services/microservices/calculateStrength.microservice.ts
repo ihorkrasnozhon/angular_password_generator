bcc133b1c1dc6fb61ccf4deb0e04beab7bb54359
ihorkrasnozhon <worldofiigor@gmail.com>
2026-04-02 00:22:40 +0200

    fonts

--- src/app/services/microservices/calculateStrength.microservice.ts
+++ src/app/services/microservices/calculateStrength.microservice.ts
@@ -2,11 +2,15 @@ export function calculateStrength(pass: string): number {
   if (!pass) return 0;
 
   let score = 1;
-  if (pass.length > 8) score += 1;
-  if (pass.length > 12) score += 1;
-  if (/[A-Z]/.test(pass)) score += 1;
+
+
+  if (pass.length < 8) return score;
+  if (pass.length > 8) score += 0.5;
+  if (pass.length > 12) score += 0.5;
+  if (pass.length > 18) score += 0.5;
+  if (/[A-Z]/.test(pass)) score += 0.5;
   if (/[0-9]/.test(pass)) score += 1;
   if (/[^A-Za-z0-9]/.test(pass)) score += 1;
 
-  return score;
+  return Math.floor(score);
 }
